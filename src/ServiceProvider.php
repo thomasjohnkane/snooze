11a30f8407f70b5e549ce328265dd1a87d167f05
atymic <atymicq@gmail.com>
2020-01-07 09:39:04 +1100

    feat: publish database migration

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -30,7 +30,11 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
             self::CONFIG_PATH => config_path('snooze.php'),
         ], 'config');
 
-        $this->loadMigrationsFrom(__DIR__.'/../migrations');
+        if (! class_exists('CreateScheduledNotificationsTable')) {
+            $this->publishes([
+                __DIR__ . '/../migrations/create_scheduled_notifications_table.stub' => database_path('migrations/' . date('Y_m_d_His') . '_create_scheduled_notifications_table.php'),
+            ], 'migrations');
+        }
 
         if ($this->app->runningInConsole()) {
             $this->commands($this->commands);

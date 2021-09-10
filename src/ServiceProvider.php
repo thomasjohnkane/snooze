76243d289592506bc093963e3f10b2dca0517cac
ricus <ricus@zero2one.co.za>
2021-09-10 13:01:40 +0200

    ensure $schedule is always available

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -17,10 +17,12 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         // Schedule base command to run every minute
         $this->app->booted(function () {
+
+            //Ensure the schedule is available if snooze is disabled but a prune age is set
+            $schedule = $this->app->make(Schedule::class);
+
             if (! config('snooze.disabled')) {
                 $frequency = config('snooze.sendFrequency', 'everyMinute');
-                $schedule = $this->app->make(Schedule::class);
-
                 if (config('snooze.onOneServer', false)) {
                     $schedule->command('snooze:send')->{$frequency}()->onOneServer();
                 } else {
